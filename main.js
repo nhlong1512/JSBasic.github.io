69e033722477f24146d0eb9cb7c013900547d2e6
nhlong1512 <84656405+nhlong1512@users.noreply.github.com>
2021-09-24 22:43:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -528,6 +528,44 @@ courses.forEach2(function(course, index, array){
 });
 
 
+//MY FILTER METHOD
+
+Array.prototype.filter2 = function(callback){
+    var output = [];
+
+    for (var index in this){
+        if(this.hasOwnProperty(index)){
+            var result = callback(this[index], index, this);
+            if(result){
+                output.push(this[index]);
+            }
+        }
+    }
+
+    return output;
+}
+
+var courses = [
+    {
+        name: 'Javascript',
+        coin: 680
+    },
+    {
+        name: 'PHP',
+        coin: 860
+    },
+    {
+        name: 'Ruby',
+        coin: 980
+    }
+];
+
+var filterCourses = courses.filter2(function(course, index, array){
+    return course.coin>700;
+})
+
+console.log(filterCourses)
+
 
 
 
