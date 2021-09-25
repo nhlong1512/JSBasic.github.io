f479b0f79a9673d642934a40a820ef9aefdd83cd
nhlong1512 <84656405+nhlong1512@users.noreply.github.com>
2021-09-25 08:35:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -568,6 +568,45 @@ console.log(filterCourses)
 
 
 
+//MY SOME METHOD
+
+Array.prototype.some2 = function(callback){
+    for( var index in this){
+        if(this.hasOwnProperty(index)){
+            if(callback(this[index], index, this)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+var courses = [
+    {
+        name: 'Javascript',
+        coin: 680,
+        isFinish: true
+    },
+    {
+        name: 'PHP',
+        coin: 860,
+        isFinish: false
+    },
+    {
+        name: 'Ruby',
+        coin: 980,
+        isFinish: false
+    }
+];
+
+
+var result = courses.some2(function(course, index, array){
+    return course.isFinish;
+});
+
+console.log(result);
+
+
 
 
 
