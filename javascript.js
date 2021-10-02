f817f5151f79e229f3d1a5fe05e6e27705e2d95b
nhlong1512 <84656405+nhlong1512@users.noreply.github.com>
2021-10-02 09:28:56 +0700

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//JSON LÀ GÌ
+//1.Nó là một định dạng dữ liệu (chuỗi)
+//2. Javascript Object Notation(JSONxcvbnm)
+//3. JSON: Number, String,  Boolean, Null, Array, Object
+//Mã hóa / Giải mã(Encode / Decode)
+//Stringify: Từ Javascript types ->JSON
+ //Parse: từ Json -> Javascript types
+
+ var a = 'true';
+ console.log(JSON.parse(a));//true
+
+ var json = '["Javascript", "PHP"]';
+console.log(JSON.parse(json));//['Javascript', 'PHP']
+
+console.log(JSON.stringify(json));//"[\"Javascript\", \"PHP\"]" ==>Dạng chuỗi
+console.log(typeof JSON.stringify(json));//string
