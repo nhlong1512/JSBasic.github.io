c55ad042f05aedd5211b45b7b8f6696813045f01
nhlong1512 <84656405+nhlong1512@users.noreply.github.com>
2021-10-04 22:02:36 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -75,3 +75,66 @@ promise
     .catch(function(err){
         console.log('err: ', err);
     });
+
+
+//THỰC HÀNH, VÍ DỤ VỀ PROMISE
+
+var users = [
+    {
+        id: 1,
+        name: 'Kien Dam'
+    },
+    {
+        id: 2, 
+        name: 'Son Dang'
+    },
+    {
+        id: 3, 
+        name: 'Hung Dam'
+    }
+];
+
+var comments = [
+    {
+        id: 1,
+        user_id: 1,
+        content: 'Anh Son chua ra video a :('
+    },
+    {
+        id: 2,
+        user_id: 2,
+        content: 'Vua ra em oi!'
+    }
+];
+
+//1. Lấy comments
+//2. Từ comments lấy ra user_id
+// từ user_id lấy ra user tương ứng
+
+//Fake API
+
+function getComments() {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(comments);
+        }, 1000)
+    });
+}
+
+function getUsersByIds(userIds){
+    return new Promise(function(resolve){
+        var result = users.filter(function(user){
+            return userIds.includes(user.id);
+        })
+        resolve(result);
+    }, 1000);
+}
+
+getComments()
+    .then(function(comments){
+        var userIds = comments.map(function(comment){
+            return comment.user_id;
+        });
+
+        console.log(userIds);
+    });
